4ae63327eccd2cf8819e18f16ce75ee797cafb40
emeemr <emelie.emretsson@svk.se>
2026-02-13 19:27:04 +0100

    added more exceptions to global exception handler

--- src/main/java/com/example/datahandler/person/advice/PersonExceptionHandler.java
+++ src/main/java/com/example/datahandler/person/advice/PersonExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.datahandler.person.advice;
 
+import com.example.datahandler.person.exception.DataLimitException;
 import com.example.datahandler.person.exception.DataReadException;
+import com.example.datahandler.person.exception.NoDataFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,6 +14,18 @@ public class PersonExceptionHandler {
     @ExceptionHandler(DataReadException.class)
     public ResponseEntity<String> handleDataReadException(DataReadException ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NoDataFoundException.class)
+    public ResponseEntity<String> handleNoDataFoundException(NoDataFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DataLimitException.class)
+    public ResponseEntity<String> handleDataLimitException(DataLimitException ex) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
                 .body(ex.getMessage());
     }
 }
